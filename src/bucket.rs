d88673bd21d426cc908d71f1719ffb4119878252
Julian Ganz <neither@nut.email>
2022-04-25 14:38:14 +0200

    bucket: add wrapper for a Vec containing sorted items
    We plan to introduce a data structure based on buckets containing
    pre-sorted items. This change introduces an internal data type for a
    single of these buckets as part of a new module.

---
+++ src/bucket.rs
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+//! Types representing individual buckets and related utilities
+
+use std::cmp::Ordering;
+
+
+/// A sorted collection of items
+///
+/// This type wraps a [Vec] of items sorted in ascending order and implements
+/// [Ord] based on its last element. The ordering amongst buckets of this type
+/// is equivalent to the ordering of the maximum item in each bucket.
+///
+/// In addition, a `SortedBucket` functions as an [Iterator] yielding (and
+/// removing) its elements from last to first, i.e. in reverse or descending
+/// order.
+pub(crate) struct SortedBucket<T: Ord>(Vec<T>);
+
+impl<T: Ord> From<Vec<T>> for SortedBucket<T> {
+    fn from(mut items: Vec<T>) -> Self {
+        items.sort_unstable();
+        Self(items)
+    }
+}
+
+impl<T: Ord> ExactSizeIterator for SortedBucket<T> {}
+
+impl<T: Ord> std::iter::FusedIterator for SortedBucket<T> {}
+
+impl<T: Ord> Iterator for SortedBucket<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        self.0.pop()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.0.len();
+        (len, Some(len))
+    }
+}
+
+impl<T: Ord> Ord for SortedBucket<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Ord::cmp(&self.0.last(), &other.0.last())
+    }
+}
+
+impl<T: Ord> PartialOrd for SortedBucket<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        PartialOrd::partial_cmp(&self.0.last(), &other.0.last())
+    }
+}
+
+impl<T: Ord> Eq for SortedBucket<T> {}
+
+impl<T: Ord> PartialEq for SortedBucket<T> {
+    fn eq(&self, other: &Self) -> bool {
+        PartialEq::eq(&self.0.last(), &other.0.last())
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+mod bucket;
+
 #[cfg(test)]
 mod tests;
 
