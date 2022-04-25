7f93e5646c580dd920072a2e40ca915406fe5343
Julian Ganz <neither@nut.email>
2022-04-25 14:35:11 +0200

    tests: add utility functions for constructing tests
    Most of our tests will be very similar: we'll generate some random
    items, feed them to our data structures and check whether the resulting
    sequence is sorted.
    
    Arguably, the `assert_sorted` function introduced with this change could
    be better expressed though `Iterator::is_sorted`. However, this function
    is currently not yet stabilized.

--- src/tests.rs
+++ src/tests.rs
@@ -3,3 +3,24 @@
 
 use super::*;
 
+use rand::Rng;
+
+
+/// Item type to use for testing
+type Item = u64;
+
+
+/// Construct an [Iterator] yielding `num` random items
+fn random_items(num: usize) -> impl Iterator<Item = Item> {
+    let mut rng = rand_pcg::Mcg128Xsl64::new(0xcafef00dd15ea5e5); // seed taken from rand_pcg docs
+    std::iter::from_fn(move || Some(rng.gen::<Item>())).take(num)
+}
+
+/// Check whether the given [Iterator] is sorted
+fn assert_sorted<T: Ord>(mut iter: impl Iterator<Item = T>) {
+    if let Some(first) = iter.next() {
+        iter.try_fold(first, |c, n| if c <= n { Some(n) } else { None })
+            .expect("Iterator does not yield sorted items");
+    }
+}
+
