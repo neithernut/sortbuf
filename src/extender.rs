642d31f7c3b868b33a4a5c4e3f8ac3dafda1adc0
Julian Ganz <neither@nut.email>
2022-05-07 23:25:47 +0200

    extender: add a fn for creating an Extender with a default bucket size
    We don't want to force our users to think about a reasonable target size
    for buckets. This convenience function offers a reasonable [TM] default.
    
    Users who may want to set another bucket size can do so via previously
    introduced functions. However, we suspect that at least some users will
    want to play around with bucket sizes relative to the default size. We
    therefore decided to expose this value, albeit we may make it private in
    the future if we feel like it.

--- src/extender.rs
+++ src/extender.rs
@@ -62,6 +62,27 @@ impl<A: BucketAccumulator> Extender<A> {
             .expect("Could not compute bucket size");
         Self::with_bucket_size(bucket_accumulator, bucket_size)
     }
+
+    /// Create a new `Extender` with a default bucket target size
+    ///
+    /// Create a new `Extender` for the given `bucket_accumulator`. [Bucket]s
+    /// committed to that [BucketAccumulator] will be of a size near a
+    /// [default bucket size](Self::DEFAULT_BUCKET_BYTESIZE).
+    pub fn with_default_bucket_size(bucket_accumulator: A) -> Self {
+        Self::with_bucket_bytesize(bucket_accumulator, Self::DEFAULT_BUCKET_BYTESIZE)
+    }
+
+    /// Default size for [Bucket]s
+    ///
+    /// This constant holds a default size for buckets, in bytes. The constant
+    /// is choosen to be reasonably large without obstructing the library's use
+    /// on smaller machines. Currently, it is set to 16MB.
+    ///
+    /// The rationale behind that value is that on a typical SBC with a quadcore
+    /// and 1GB of ram, it should be possible to accumulate items into buckets
+    /// for multiple (e.g. 3) [BucketAccumulator]s on all cores without
+    /// exhausting memory (or running into overcommitting).
+    pub const DEFAULT_BUCKET_BYTESIZE: usize = 16*1024*1024;
 }
 
 impl<A: BucketAccumulator> Extend<A::Item> for Extender<A> {
