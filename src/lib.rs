75d9b6a4df05c24c02adc319a5e5fd7b0780ea06
Julian Ganz <neither@nut.email>
2022-05-07 21:30:46 +0200

    SortBuf: add data structure for preparing sorted iteration
    This change introduces the most prominent, user-facing type, which also
    gives this library its name. It currently doesn't feature any interface
    for adding new items to the buffer. This will be introduced by future
    changes after a bit more ground work.

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,24 @@ mod iter;
 #[cfg(test)]
 mod tests;
 
+
+/// Data structure for preparing a large number of items for sorted iteration
+///
+/// # Other notes
+///
+/// The omission of an implementation of [Clone] for this type is on purpose, as
+/// it is meant for large amounts of data.
+#[derive(Default)]
+pub struct SortBuf<T: Ord> {
+    buckets: std::collections::BinaryHeap<bucket::SortedBucket<T>>,
+}
+
+impl<T: Ord> IntoIterator for SortBuf<T> {
+    type Item = T;
+    type IntoIter = iter::Iter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.buckets.into()
+    }
+}
+
