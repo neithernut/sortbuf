568d9a4bd1c39e53f9ece44982fb0a3ac86c87c7
Julian Ganz <neither@nut.email>
2022-05-24 19:27:12 +0200

    SortBuf: add additional documentation
    Now that we introduced `Extender`s, we can finally point users to that
    trait.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,15 @@ pub use extender::{BucketAccumulator, Extender};
 
 /// Data structure for preparing a large number of items for sorted iteration
 ///
+/// This data structure buffers items for later iteration in descending order.
+/// New items are inserted via an [Extender] which has to be constructed
+/// separately for a given buffer. Once all (or sufficently many) items are
+/// inserted, [IntoIterator] may be used for iterating over these items in
+/// descending order (according to the items' implementation of [Ord]).
+///
+/// For ascending iteration, users need to wrap items in [std::cmp::Reverse] and
+/// unwrap them during the final iteration.
+///
 /// # Other notes
 ///
 /// The omission of an implementation of [Clone] for this type is on purpose, as
