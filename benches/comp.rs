37aa8fb7645c2210735dd05fdb7e2927f3b921a6
Julian Ganz <neither@nut.email>
2022-05-24 21:44:37 +0200

    benches: add fns for creating an Iterator over random items
    In our comparison benchmark, we want to compare performance metrics of
    different approaches for sorting large amounts of data. In order to do
    so, we need to generate such data. While the generation should be fast
    if should also yield sufficiently random data.
    
    We already use a PCG implementation for our tests which is supposedly
    fast in generating random values. We decided to use the same generation
    approach for our benchmarks. A dummy generate-and-discard benchmark will
    help assert this to some extend by providing a baseline.

--- benches/comp.rs
+++ benches/comp.rs
@@ -6,6 +6,21 @@ fn main() {
 }
 
 
+/// Create an iterator over random items
+fn random_items(num: usize) -> impl Iterator<Item = u64> {
+    random_items_with_seed(num, 0xcafef00dd15ea5e5) // seed taken from rand_pcg docs
+}
+
+
+/// Create an iterator over random items with a given seed
+fn random_items_with_seed(num: usize, seed: u128) -> impl Iterator<Item = u64> {
+    use rand::Rng;
+
+    let mut rng = rand_pcg::Mcg128Xsl64::new(seed);
+    std::iter::from_fn(move || Some(rng.gen())).take(num)
+}
+
+
 /// Snapshot of the current resource usage and time
 #[derive(Copy, Clone, Debug)]
 struct Snapshot {
