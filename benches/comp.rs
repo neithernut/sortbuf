67c9e6a73856885b828c679a685bb056771b7ef4
Julian Ganz <neither@nut.email>
2022-05-24 21:45:21 +0200

    benches: add fn for taking measurements
    In our comparison benchmark, we want to compare performance metrics of
    different approaches for sorting large amounts of data. These different
    approaches involve values of different types constructed by different
    means. However, all cases involve a creating an `Iterator` which is then
    drained.
    
    This change introduces a function for performing and measuring these two
    steps.

--- benches/comp.rs
+++ benches/comp.rs
@@ -1,8 +1,41 @@
 //! Comparison between different kinds of sorting buffers
 
+use std::time::Duration;
+
 
 fn main() {
     // TODO: impl
+}
+
+
+/// Benchmark a single sorting buffer
+///
+/// This function accepts a function which is expected to construct a sorted
+/// buffer with random data as well as the number of items with which to fill
+/// the buffer.
+///
+/// The function will construct the sorted buffer, measuring the resources used
+/// suring the process. It will then drain the buffer, again measuring the
+/// resource usage. Both resource usages are then returned to the caller
+/// together with the overall wallclock time.
+fn bench_func<I>(mk_iter: impl Fn(usize) -> I, items: usize) -> (Duration, Diff, Diff)
+where I: IntoIterator<Item = u64>,
+{
+    let t0 = std::time::Instant::now();
+    let s0 = Snapshot::new();
+    let iter = mk_iter(items);
+
+    let s1 = Snapshot::new();
+    let mut iter = iter.into_iter();
+    if let Some(first) = iter.next() {
+        // TODO: use a black box of some sort to prevent the draining to be optimized out
+        iter.try_fold(first, |c, n| if c <= n { Some(n) } else { None })
+            .expect("Iterator does not yield sorted items");
+    }
+
+    let s2 = Snapshot::new();
+
+    (t0.elapsed(), s1.diff(s0), s2.diff(s1))
 }
 
 
