c5340dd1de1450ae88a572b5df3c058cce5e6de9
Julian Ganz <neither@nut.email>
2022-05-24 20:37:59 +0200

    benches: add types representing resource usage snapshots and differences
    These types will simplify the retrieval and comparison of resource usage
    data. For now, we just add an empty implementation. Future commits will
    add fields representing different resources.

--- benches/comp.rs
+++ benches/comp.rs
@@ -5,3 +5,27 @@ fn main() {
     // TODO: impl
 }
 
+
+/// Snapshot of the current resource usage and time
+#[derive(Copy, Clone, Debug)]
+struct Snapshot {
+}
+
+impl Snapshot {
+    /// Create a new [Snapshot]
+    pub fn new() -> Self {
+        Self{}
+    }
+
+    /// Compare this [Snapshot] to an earlier one
+    pub fn diff(&self, older: Self) -> Diff {
+        Diff{}
+    }
+}
+
+
+/// The difference between two [Snapshot]s
+#[derive(Copy, Clone, Debug)]
+struct Diff {
+}
+
