bfaeee4655c9a7947f66e6ab7ae878de6fbdb678
Julian Ganz <neither@nut.email>
2022-06-18 23:37:39 +0200

    error: add an error type for failures to insert items
    We'll use this type to convey (re-)allocation failures in the future.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,32 @@
 // SPDX-License-Identifier: MIT
 //! Types and utilities related to error handling and reporting
 
+use std::collections::TryReserveError;
+use std::error::Error;
+use std::fmt;
+
+
+/// Insertion error
+///
+/// This type conveys errors occuring during the insertion of items to a buffer.
+#[derive(Debug)]
+pub struct InsertionError(TryReserveError);
+
+impl From<TryReserveError> for InsertionError {
+    fn from(inner: TryReserveError) -> Self {
+        Self(inner)
+    }
+}
+
+impl Error for InsertionError {
+    fn cause(&self) -> Option<&(dyn Error + 'static)> {
+        Some(&self.0)
+    }
+}
+
+impl fmt::Display for InsertionError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt.write_str("Could not add items to accumulator")
+    }
+}
+
